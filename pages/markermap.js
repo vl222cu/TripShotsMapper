f528012601c484c2d8f9a069a3cd8437310c23c3
OpenShift guest <vl222cu@student.lnu.se>
2014-04-27 13:23:46 -0400

    lagt till markerfunktion

---
+++ pages/markermap.js
@@ -0,0 +1,37 @@
+var google;
+
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(59.999999, 14.9999999),
+    zoom: 2
+  };
+
+  var map = new google.maps.Map(document.getElementById('markmap-canvas'),
+    mapOptions);
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.POLYLINE,
+      ]
+    },
+    markerOptions: {
+      icon: '../pics/pinkflag.png'
+    },
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+  drawingManager.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
